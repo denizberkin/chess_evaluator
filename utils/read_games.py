c59bb713a930835479327b4ae5e1f713343d41e7
denizberkin <berkindeniz2000@gmail.com>
2024-11-06 10:24:08 +0300

    update read_games and serialize py location to utils

---
+++ utils/read_games.py
@@ -0,0 +1,104 @@
+""" 
+scripts to check current game state for now? 
+read a pgn file and print game
+"""
+
+import os
+from typing import *
+import logging
+
+import numpy as np
+import chess
+import chess.pgn as pgn
+from tqdm import tqdm
+
+from utils.serialize import BoardState
+
+
+# Configure logging
+logging.basicConfig(filename='game_processing.log', level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+# score from white's perspective
+RESULTS = {
+    "1-0": 1,
+    "0-1": -1,
+    "1/2-1/2": 0,
+}
+
+
+def read_games_from_pgn(fn: str) -> List[pgn.Game]:
+    """ pgn.read_game reads one game per call!"""
+    games = []
+    file_size = os.path.getsize(fn)
+    pgn_bytes = open(fn)
+    
+    with open(fn) as pgn_bytes:
+        pbar = tqdm(total=file_size, desc="Reading game file", unit="byte")
+        game = pgn.read_game(pgn_bytes)
+        while game is not None:
+            try:
+                logging.DEBUG(f"Loaded game: {game.headers["White"]} vs {game.headers["Black"]}")
+                games.append(game)
+            except Exception as e:
+                logging.error(f"Error reading game: {e}")
+                break
+            
+            # update pbar
+            pbar.update(pgn_bytes.tell() - pbar.n)
+            game = pgn.read_game(pgn_bytes)
+            
+        pbar.close()
+    
+    logging.info(f"Loaded {len(games)} games from {fn}\n")
+    return games
+
+
+def configure_dataset(games: List[pgn.Game], 
+                      limit=None
+                      ) -> Tuple[np.ndarray, np.ndarray]:
+    """ 
+    dataset generation for train, val, test
+    Q: Should I use yield as this may get heavy with a large set?
+    Returns: 
+        - X: list of board states
+        - y: list of scores
+    """
+    X, Y = [], []
+    num_samples = 0
+    
+    for game in tqdm(games, desc="Processing games", unit="game"):
+        result = game.headers["Result"] if "Result" in game.headers else None
+        if result not in RESULTS:
+            logging.warning(f"Skipping game due to invalid result: {game.headers}")
+            continue  # skip game if result is broken somehow
+        
+        y = RESULTS[result]  # score from white's perspective
+        board = chess.Board()
+        
+        for move in game.mainline_moves():
+            # unfortunately, even the bad moves of the winning side are appointed as positive
+            board.push(move)
+            x = BoardState(board).board2np()
+            X.append(x)
+            Y.append(y)
+            
+            if limit is not None and len(Y) >= limit:
+                logging.info(f"Reached limit of {limit} samples, stopping!")
+                return np.array(X), np.array(Y)
+            
+            num_samples += 1
+    
+    return np.array(X), np.array(Y)
+            
+
+
+if __name__ == "__main__":
+    fn = "data/Nakamura.pgn"
+    games = read_games_from_pgn(fn)
+    
+    X, Y = configure_dataset(games)
+    
+    np.savez("processed/Nakamura.npz", X=X, Y=Y)
+    
+    
---
+++ utils/serialize.py
@@ -0,0 +1,51 @@
+from typing import *
+
+import chess
+import numpy as np
+
+# order stated in chess.Piece.symbol(): capital for white, lowercase for black
+BOARD_SIZE = 8
+
+# 0-> empty square, 7 and 14 are for castling
+PIECE_ENCODINGS = {"P": 1, "N": 2, "B": 3, "R": 4, "Q": 5, "K": 6,
+                   "p": 8, "n": 9, "b": 10, "r": 11, "q": 12, "k": 13}
+
+
+class BoardState:
+    def __init__(self, board: chess.Board=None) -> Self:
+        self.board = board if board is not None else chess.Board()
+        
+    def board2np(self) -> np.ndarray:
+        if not self.board.is_valid():
+            raise ValueError("Invalid board state")
+        
+        # square board
+        current_board = np.zeros(BOARD_SIZE ** 2, dtype=np.int8)
+        for i in range(BOARD_SIZE ** 2):
+            piece: chess.Piece = self.board.piece_at(i)
+            if piece is not None:  # else already defined as 0
+                current_board[i] = PIECE_ENCODINGS[piece.symbol()]
+
+        # set castling rights
+        current_board[0] = 7 if self.board.has_kingside_castling_rights(chess.WHITE) else 0
+        current_board[7] = 7 if self.board.has_queenside_castling_rights(chess.WHITE) else 0
+        current_board[56] = 14 if self.board.has_kingside_castling_rights(chess.BLACK) else 0
+        current_board[63] = 14 if self.board.has_queenside_castling_rights(chess.BLACK) else 0
+            
+        # en passant
+        if self.board.ep_square is not None:
+            current_board[self.board.ep_square] = 15
+            
+        # XXX: change this block in case I want to implement a different representation, 
+        # one hot for different pieces might be the case as alpha zero does use a similar implementation
+        
+        binary_board = np.zeros((5, 8, 8), dtype=np.int8)  # see readme for shape explanation
+        
+        # need to reshape the current board to 8x8 before applying binary ops
+        current_board = current_board.reshape(8, 8)
+        for i in range(4):
+            binary_board[i] = (current_board >> i) & 1  # shift right i times, then bitwise AND
+        binary_board[4] = (self.board.turn * 1.0) # 1.0 for white, 0.0 for black
+        
+        # END
+        return current_board  # 5, 8, 8 np array
